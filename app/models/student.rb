5df87377e762330f617f10cf849e668e0461ebbe
Zachary Davy <zacharyjdavy@gmail.com>
2014-01-23 14:12:27 -0600

    Write Student validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,12 +2,17 @@ require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
 # implement your Student model here
+  validates :email, :format => {:with => /.+@.+\..{2,}/}
+  validates :email, :uniqueness => true
+  validates :age, :numericality => {:greater_than_or_equal_to => 5}
+  validates :phone, :length => {:minimum => 10}
+
   def name
-    "#{self.first_name}, #{self.last_name}"
+    "#{self.first_name} #{self.last_name}"
   end
 
   def age
-    Date.today.year - self.birthday.year
+    ((Date.today - self.birthday) / 365).to_i
   end
 
 end
